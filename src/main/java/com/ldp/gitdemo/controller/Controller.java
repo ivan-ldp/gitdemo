a93e5a61c30f4ecf364948bc0f78cf010e5531d3
ivan <Ivan_ldp@163.com>
2019-08-30 13:19:19 +0800

    添加端口号8080 进行了测试

--- src/main/java/com/ldp/gitdemo/controller/Controller.java
+++ src/main/java/com/ldp/gitdemo/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.ldp.gitdemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    @ResponseBody
     @RequestMapping("/test")
     public String test(){
         return "hello";
