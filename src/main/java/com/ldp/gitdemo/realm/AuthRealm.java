0e77cefe8706687f16608b9b51e53e1d871d021f
ivan <Ivan_ldp@163.com>
2019-08-31 11:28:12 +0800

    用户授权 realm

--- src/main/java/com/ldp/gitdemo/realm/AuthRealm.java
+++ src/main/java/com/ldp/gitdemo/realm/AuthRealm.java
@@ -1,9 +1,12 @@
 package com.ldp.gitdemo.realm;
 
+import com.ldp.gitdemo.pojo.Resource;
+import com.ldp.gitdemo.pojo.Role;
 import com.ldp.gitdemo.pojo.User;
 import com.ldp.gitdemo.service.UserService;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +25,20 @@ public class AuthRealm extends AuthorizingRealm {
      */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+        //取出用户
+        User user = (User)principalCollection.getPrimaryPrincipal();
+        //通常会创建SimpleAuthorizationInfo对象  放权限与角色信息
+        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+        for (Role role:user.getRoles()){
+            //将角色标识加入到SimpleAuthorizationInfo对象当中
+            info.addRole(role.getRoleCode());
+            for (Resource resource:role.getResources()){
+                //将资源标识与资源路径加入到SimpleAuthorizationInfo对象当中
+               info.addStringPermission(resource.getResCode());
+               info.addStringPermission(resource.getUrl());
+            }
+        }
+        return info;
     }
 
     /**
@@ -41,6 +57,9 @@ public class AuthRealm extends AuthorizingRealm {
             throw new UnknownAccountException("用户名或密码错误");
         }
         //通常会创建SimpleAuthenticationInfo对象进行数据封装并返回
+        //认证信息
+        //密码
+        //当前realm对象的name
         SimpleAuthenticationInfo info=new SimpleAuthenticationInfo(user,user.getPassword(),this.getName());
         return info;
     }
